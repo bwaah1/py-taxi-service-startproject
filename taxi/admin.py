ea7bd5bcc2c93b8895d8ccb631fbead8f797997b
bwaah1 <chykivan@gmail.com>
2024-05-07 12:52:59 +0300

    Fix quotes

--- taxi/admin.py
+++ taxi/admin.py
@@ -5,22 +5,22 @@ from taxi.models import Driver, Manufacturer, Car
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ('license_number',)
+    list_display = UserAdmin.list_display + ("license_number",)
     fieldsets = UserAdmin.fieldsets + (
-        ('Additional info', {'fields': ('license_number',)}),
+        ("Additional info", {"fields": ("license_number",)}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Additional info', {'fields': ('license_number',)}),
+        ("Additional info", {"fields": ("license_number",)}),
     )
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
     search_fields = [
-        'model',
+        "model",
     ]
     list_filter = [
-        'manufacturer',
+        "manufacturer",
     ]
 
 
--- taxi/models.py
+++ taxi/models.py
@@ -14,11 +14,11 @@ class Manufacturer(models.Model):
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(
-        Manufacturer, on_delete=models.CASCADE, related_name='cars'
+        Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(
         settings.AUTH_USER_MODEL,
-        related_name='cars'
+        related_name="cars"
     )
 
     def __str__(self) -> str:
